d3fbe7c6ac3fe161bd4c6b414aefc46b73511bb8
Patty Liu <pattylliu@gmail.com>
2019-08-01 02:04:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =
+[
+  [0, 1, 2], #top row
+  [3, 4, 5], #mid row
+  [6, 7, 8], #bot row
+  [0, 3, 6], #1st column
+  [1, 4, 7], #2nd column
+  [2, 5, 8], #3rd column
+  [0, 4, 8], #1st diagonal
+  [2, 4, 6] #2nd diagonal
+]
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# won?
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_combination.each do |win_index|
+      win_index.each do |position|
+        position = board[win_index]
+          if position == " "
+            return false
+          else
+            false
+        end
+      end
+    end
+  end
+end
